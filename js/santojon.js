231c613b99e2278c96c1ad5275c207c95fdd8458
Jonathan Santos <jgs@cin.ufpe.br>
2015-11-04 18:43:14 -0300

    update app

--- js/santojon.js
+++ js/santojon.js
@@ -1,39 +1,9 @@
-// js functions
-function hideIndexelements() {
-	$('#index-link').fadeOut();
-
-	setTimeout(function() {
-		$('#index-content').fadeOut();
-
-		setTimeout(function() {
-			$('#navbar-bottom').fadeIn();
-
-			setTimeout(function() {
-				$('#login-content').fadeIn();
-			}, 300);
-		}, 500);
-	}, 300);
-}
-
-function showHome(element) {
-	$(element).fadeOut();
-
-	setTimeout(function() {
-		$('.login-bg').fadeOut();
-
-		setTimeout(function() {
-			$(element).removeClass('navbar-fixed-bottom');
-			$(element).fadeIn();
-
-			setTimeout(function() {
-				$('#home-content').fadeIn();
-			}, 500);
-		}, 500);
-	}, 500);
-}
-
-function rolada(element) {
-	$('html, body').animate({
-		scrollTop: $(element).offset().top
-	}, 2000);
+/* Script to scroll content */
+function scrollToID(container, elementOffset, id) {
+    var offset = elementOffset;
+    var targetOffset = $(id).offset().top - offset;
+    if (container != "html,body") {
+        targetOffset = targetOffset + $(container).scrollTop();
+    }
+    $(container).animate({scrollTop: targetOffset}, 500);
 }
